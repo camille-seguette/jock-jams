2a11cf1173ebfdb8367eaa0c56ec977f5c25797c
Camille <camilleseguette@gmail.com>
2017-04-19 22:17:43 -0700

    Assignment 22 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,36 +1,14 @@
-var pointsArray = document.getElementsByClassName('point');
+var animatePoints = function() {
 
-var animatePoints = function(points) {
+    var points = document.getElementsByClassName('point');
 
-     var revealFirstPoint = function() {
-         points[0].style.opacity = 1;
-         points[0].style.transform = "scaleX(1) translateY(0)";
-         points[0].style.msTransform = "scaleX(1) translateY(0)";
-         points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
-
-     var revealSecondPoint = function() {
-         points[1].style.opacity = 1;
-         points[1].style.transform = "scaleX(1) translateY(0)";
-         points[1].style.msTransform = "scaleX(1) translateY(0)";
-         points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
-
-     var revealThirdPoint = function() {
-         points[2].style.opacity = 1;
-         points[2].style.transform = "scaleX(1) translateY(0)";
-         points[2].style.msTransform = "scaleX(1) translateY(0)";
-         points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
-
-     revealFirstPoint();
-     revealSecondPoint();
-     revealThirdPoint();
-
- };
-
- window.onload = function() {
-   window.addEventListener('scroll', function(event) {
-     console.log(event);
-   }
- }
+    var revealPoint = function() {
+        for(var i = 0; i < points.length; i++){
+            points[i].style.opacity = 1;
+            points[i].style.transform = "scaleX(1) translateY(0)";
+            points[i].style.msTransform = "scaleX(1) translateY(0)";
+            points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+       }
+    };
+    revealPoint(points);
+};
