10e3d7b5964c8a3ec3264c5dafb75cd46953fee4
Camille <camilleseguette@gmail.com>
2017-04-20 16:58:54 -0700

    Assignment 23 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,15 +1,14 @@
 var pointsArray = document.getElementsByClassName('point');
 
+var revealPoint = function(point) {
+        point.style.opacity = 1;
+        point.style.transform = "scaleX(1) translateY(0)";
+        point.style.msTransform = "scaleX(1) translateY(0)";
+        point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
+
 var animatePoints = function(points) {
-    var revealPoint = function() {
-        for(var i = 0; i < points.length; i++){
-            points[i].style.opacity = 1;
-            points[i].style.transform = "scaleX(1) translateY(0)";
-            points[i].style.msTransform = "scaleX(1) translateY(0)";
-            points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-       }
-    };
-    revealPoint(points);
+  forEach(points, revealPoint);
 };
 
 window.onload = function() {
