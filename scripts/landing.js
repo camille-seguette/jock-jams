816761734588381ee1d692393fd8f1dd99120df5
Camille <camilleseguette@gmail.com>
2017-04-19 23:47:02 -0700

    Checkpoint 23 DOM Scripting: Events completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,6 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-    var points = document.getElementsByClassName('point');
-
+var animatePoints = function(points) {
     var revealPoint = function() {
         for(var i = 0; i < points.length; i++){
             points[i].style.opacity = 1;
@@ -12,3 +11,18 @@ var animatePoints = function() {
     };
     revealPoint(points);
 };
+
+window.onload = function() {
+  if (window.innerHeight > 950) {
+    animatePoints(pointsArray);
+  }
+
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+  window.addEventListener('scroll', function(event) {
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+      animatePoints(pointsArray);
+    }
+  });
+}
